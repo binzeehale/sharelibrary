fe35f782212e5fda009085e2b56950b4a36f3c17
binson <binzeehale@gmail.com>
2013-11-05 00:28:27 +0800

    add sql file

---
+++ slib.sql
@@ -0,0 +1,87 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.49-community : Database - sharelibrary2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sharelibrary2` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sharelibrary2`;
+
+/*Table structure for table `docs` */
+
+DROP TABLE IF EXISTS `docs`;
+
+CREATE TABLE `docs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(1024) NOT NULL,
+  `type` int(11) DEFAULT NULL COMMENT '1:dir;2:file',
+  `path` varchar(1024) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT '0',
+  `mark_id` int(11) DEFAULT '0',
+  `tmp_id` int(11) DEFAULT '0',
+  `user_id` int(11) DEFAULT NULL,
+  `last_update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `docs` */
+
+/*Table structure for table `mark` */
+
+DROP TABLE IF EXISTS `mark`;
+
+CREATE TABLE `mark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `lat` varchar(128) DEFAULT NULL COMMENT '纬度',
+  `lng` varchar(128) DEFAULT NULL COMMENT '经度',
+  `user_id` int(11) DEFAULT NULL,
+  `last_update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `mark` */
+
+/*Table structure for table `tmp` */
+
+DROP TABLE IF EXISTS `tmp`;
+
+CREATE TABLE `tmp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(1024) DEFAULT NULL,
+  `path` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tmp` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `weight` int(11) NOT NULL,
+  `last_update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`weight`,`last_update_time`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3',2,'2013-09-22 23:52:21');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
